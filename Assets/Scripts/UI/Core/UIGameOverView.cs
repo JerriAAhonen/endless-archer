2386ef59152d4c723b36c53f7aa51bd97766cf82
Jerri Ahonen <jerri.ahonen@gmail.com>
2024-04-07 14:12:17 +0300

    Add UI sequence to game over view

--- Assets/Scripts/UI/Core/UIGameOverView.cs
+++ Assets/Scripts/UI/Core/UIGameOverView.cs
@@ -6,9 +6,14 @@ using UnityEngine.UI;
 public class UIGameOverView : UICoreViewBase
 {
 	[Space]
-	[SerializeField] private TextMeshProUGUI score;
-	[SerializeField] private TextMeshProUGUI highscore;
+	[SerializeField] private GameObject title;
+	[SerializeField] private GameObject score;
+	[SerializeField] private TextMeshProUGUI scoreAmount;
+	[Space]
+	[SerializeField] private GameObject highscore;
+	[SerializeField] private TextMeshProUGUI highscoreAmount;
 	[SerializeField] private UIRainbowText highscoreRainbow;
+	[Space]
 	[SerializeField] private Button buttonContinue;
 	[Space]
 	[SerializeField] private float scoreAnimDur = 1f;
@@ -24,19 +29,45 @@ public class UIGameOverView : UICoreViewBase
 
 	public void Show(int score, int highscore, bool isNewHighscore, Action onContinue)
 	{
+		HideAll();
 		SetVisible(true);
 
-		this.score.text = "0";
-		this.highscore.text = "0";
-		UIAnimUtil.AnimateNumber(this.score, 0, score, scoreAnimDur);
+		scoreAmount.text = "0";
+		highscoreAmount.text = "0";
+
+		new Sequence.Builder()
+			.AddElement(0, () => Show(title))
+			.AddElement(100, () => Show(this.score))
+			.AddElement(100, () => Show(scoreAmount.gameObject))
+			.AddElement(100, () => Show(this.highscore))
+			.AddElement(100, () => Show(highscoreAmount.gameObject))
+			.AddElement(100, () => Show(buttonContinue.gameObject))
+			.Start();
+
+		UIAnimUtil.AnimateNumber(scoreAmount, 0, score, scoreAnimDur);
 		var highscoreAnimDur = isNewHighscore ? scoreAnimDur * newHighscoreAnimDurMultiplier : scoreAnimDur;
-		UIAnimUtil.AnimateNumber(this.highscore, 0, highscore, highscoreAnimDur, () =>
+		UIAnimUtil.AnimateNumber(highscoreAmount, 0, highscore, highscoreAnimDur, () =>
 		{
 			if (isNewHighscore)
 				highscoreRainbow.Animate();
 		});
 
 		continueCallback = onContinue;
+
+		void HideAll()
+		{
+			title.transform.localScale = Vector3.zero;
+			this.score.transform.localScale = Vector3.zero;
+			scoreAmount.transform.localScale = Vector3.zero;
+			this.highscore.transform.localScale = Vector3.zero;
+			highscoreAmount.transform.localScale = Vector3.zero;
+			buttonContinue.transform.localScale = Vector3.zero;
+		}
+
+		void Show(GameObject go)
+		{
+			UIAnimUtil.AnimateScale(go, Vector3.one, LeanTweenType.easeOutBack, 0.1f);
+		}
 	}
 
 	private void OnContinueClicked()
